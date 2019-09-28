bff458c6ea5124365bf207ff1578f7bdbbabc184
Елья <piragof@gmail.com>
2019-09-28 13:38:52 +0400

    правка

--- js/setup.js
+++ js/setup.js
@@ -60,16 +60,45 @@ for (var i = 0; i < wizards.length; i++) {
 similarListElement.appendChild(fragment);
 
 
-//  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+// userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
 //  ОТКРЫТИЕ ЗАКРЫТИЕ ОКНА НАСТРОЙКИ ПЕРСОНАЖА
 
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
 
-var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
 var setupClose = setup.querySelector('.setup-close');
+
+setupOpen.addEventListener('click', function () {
+  setup.classList.remove('hidden');
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    setup.classList.remove('hidden');
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  setup.classList.add('hidden');
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    setup.classList.add('hidden');
+  }
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    setup.classList.add('hidden');
+  }
+});
+
+document.addEventListener('keydown', onPopupEscPress);
+document.removeEventListener('keydown', onPopupEscPress);
 
 var onPopupEscPress = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
@@ -79,12 +108,10 @@ var onPopupEscPress = function (evt) {
 
 var openPopup = function () {
   setup.classList.remove('hidden');
-  document.addEventListener('keydown', onPopupEscPress);
 };
 
 var closePopup = function () {
   setup.classList.add('hidden');
-  document.removeEventListener('keydown', onPopupEscPress);
 };
 
 setupOpen.addEventListener('click', function () {
