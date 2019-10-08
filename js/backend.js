7940bacb80accb4552ff926508df081d0de7f9de
elyaperagov <piragof@gmail.com>
2019-10-08 07:55:22 +0300

    backend

---
+++ js/backend.js
@@ -0,0 +1,17 @@
+'use strict';
+
+(function () {
+  window.backend = {
+    load: function (onLoad, onError) {
+    var URL = 'https://1510.dump.academy/code-and-magick/data';
+    }
+  }
+  window.backend = {
+    save: function (data, onLoad, onError) {
+
+    }
+  }
+
+
+
+})();
