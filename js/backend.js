09e5e845301d3eed3752a54013e1d1167d1b9afb
Елья <piragof@gmail.com>
2019-10-08 19:42:47 +0400

    backend

--- js/backend.js
+++ js/backend.js
@@ -3,15 +3,81 @@
 (function () {
   window.backend = {
     load: function (onLoad, onError) {
-    var URL = 'https://1510.dump.academy/code-and-magick/data';
-    }
-  }
-  window.backend = {
+      var URL = 'https://1510.dump.academy/code-and-magick/data';
+      //   var CALLBACK_NAME = 'loadUsersWizards';
+      //   var loader = document.createElement('script');
+      //   loader.src = URL + '?callback=' + CALLBACK_NAME;
+
+
+      //   loader.addEventListener('error', function () {
+      //     onError('Произошла ошибка!');
+      //   });
+
+      //   loader.addEventListener('timeout', function () {
+      //     onError('Истек таймаут ожидания ответа!');
+      //   });
+
+      //   document.body.appendChild(loader);
+      // },
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+            onLoad(xhr.response);
+        else {
+          onError('Произошла ошибка: ' + xhr.status + ' ' + xhr.responseText);
+          }
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка!');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Истек таймаут ожидания ответа!');
+      });
+
+      xhr.timeout = 10000;
+      xhr.open('GET', URL);
+      xhr.send();
+    },
+
     save: function (data, onLoad, onError) {
+      var URL = 'https://1510.dump.academy/code-and-magick';
+      var xhr = new XMLHttpRequest();
 
+      xhr.responseType = 'json';
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+            onLoad(xhr.response);
+        else {
+          onError('Произошла ошибка: ' + xhr.status + ' ' + xhr.responseText);
+          }
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Ошибка соединения с сервером');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Истек таймаут соединения с сервером');
+      });
+
+      xhr.timeout = 10000;
+      xhr.open('POST', URL);
+      xhr.send(data);
     }
-  }
-
-
-
+  };
 })();
+
+
+var form = userDialog.querySelector('.setup-wizard-form');
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), function (response) {
+      userDialog.classList.add('hidden');
+    });
+    evt.preventDefault();
+  });
